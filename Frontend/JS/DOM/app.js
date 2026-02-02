32ac246c51e87e7f76d48c8fe6ca8359964c399c
MERN <pranavrawat0514@gmail.com>
2026-02-02 13:43:49 +0530

    Learned Dom and its 4 pillars

---
+++ Frontend/JS/DOM/app.js
@@ -0,0 +1,18 @@
+var h=document.querySelector('h1');
+console.log(h);
+var h1=document.getElementById('id');
+console.log(h1);
+var button=document.querySelector('button');
+var box=document.querySelector('.box');
+button.addEventListener('click',function(){
+    box.style.backgroundColor='green'
+})
+
+
+h.addEventListener('click',function(){
+    console.log('clicked on h');
+    h.innerHTML="kuch bhi";
+    h.style.color="red";
+    h.style.backgroundColor="yellow";
+})
+
