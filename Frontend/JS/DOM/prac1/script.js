f0256f6578dff6d10276f045bf4c9db6d0c05221
MERN <pranavrawat0514@gmail.com>
2026-02-08 09:05:09 +0530

    Event Bubbling

--- Frontend/JS/DOM/prac1/script.js
+++ Frontend/JS/DOM/prac1/script.js
@@ -1,3 +1,14 @@
 var h1=document.querySelector("h1");
 h1.innerHTML="kuch bhi"
 h1.setAttribute('id','hero')
+
+var img1=document.querySelector('#img1')
+var img2=document.querySelector('#img2')
+
+var btn=document.querySelector('button')
+btn.addEventListener('click',function(){
+    var img1src=img1.getAttribute('src')
+    var img2src=img2.getAttribute('src')
+    img1.setAttribute('src',img2src)
+    img2.setAttribute('src',img1src)
+})
