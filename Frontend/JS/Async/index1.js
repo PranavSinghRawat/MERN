fd39572e29e2ee05a1ff8b2a6903628bda008d76
MERN <pranavrawat0514@gmail.com>
2026-03-05 14:27:02 +0530

    Learned some Async task with fetch function

---
+++ Frontend/JS/Async/index1.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click',function(){
+    fetch('https://api.github.com/users/async')
+    .then((raw)=>raw.json())
+    .then((data)=>{
+        console.log(data);
+    })
+})
