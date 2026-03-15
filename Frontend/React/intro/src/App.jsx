21c053fec3db65a786dfd2a020112c6123ab09ae
MERN <pranavrawat0514@gmail.com>
2026-03-15 15:13:59 +0530

    Started with EventHandler in React

--- Frontend/React/intro/src/App.jsx
+++ Frontend/React/intro/src/App.jsx
@@ -1,11 +1,24 @@
+
+const handleclick=()=>{
+  alert('button clicked')
+}
+const handleparamclick=(msg)=>{
+  alert(msg);
+}
+const wrapper=()=>{
+  handleparamclick('kya ladle padhle ');
+}
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
+    <> 
+      <div>App</div>
+      <div>hello world</div>
+      <button onClick={handleclick}>click</button>
+
+      <button onClick={wrapper}>click(param)</button>
+    </>
+    );
 }
 
 export default App;
-export const x='hello';
-export const y='world';
+
