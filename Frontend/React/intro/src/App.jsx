d566ef2b3ee6c2d3d84ead43b55952e7a3bf1a01
MERN <pranavrawat0514@gmail.com>
2026-03-16 12:19:18 +0530

    Practice React Hooks and two way of binding

--- Frontend/React/intro/src/App.jsx
+++ Frontend/React/intro/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const handleclick=()=>{
   alert('button clicked')
@@ -9,6 +10,25 @@ const wrapper=()=>{
   handleparamclick('kya ladle padhle ');
 }
 const App = () => {
+
+  const profiles=[
+    {name: 'pranav',age: 20},
+    {name: 'ayushman',age: 21},
+    {name: 'atharv',age: 18}
+  ];
+  const updatedprofiles=profiles.map((profiles,index)=>{
+    return(
+      <li>
+        <span>Name: {profiles.name} </span>
+        <small>Age: {profiles.age}</small>
+      </li>
+    )
+  });
+
+  const[username,setusername]=useState('pranav');
+  const changehandler=()=>{
+    setusername('atharv');
+  }
   return (
     <> 
       <div>App</div>
@@ -16,6 +36,13 @@ const App = () => {
       <button onClick={handleclick}>click</button>
 
       <button onClick={wrapper}>click(param)</button>
+
+      <h1>Rendering json</h1>
+      <ol>{updatedprofiles}</ol>
+
+      <h2>Username</h2>
+      <h3>{username}</h3>
+      <button onClick={changehandler}>change username</button>
     </>
     );
 }
