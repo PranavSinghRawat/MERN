2d1839ca9861c633be0debd1a37ac1358cb7c834
MERN <pranavrawat0514@gmail.com>
2026-03-25 15:30:18 +0530

    Practice crud operation

--- Frontend/React/project1/src/App.jsx
+++ Frontend/React/project1/src/App.jsx
@@ -1,19 +1,48 @@
+import { useState } from "react"
+import { nanoid } from "nanoid"
+
 const App = () => {
+
+  const [todo, settodos] = useState([
+    { id: 1, title: "kaam karle", isCompleted: false }
+  ])
+  const [title, settitle] = useState("")
+  const Submithandler = (e) => {
+    e.preventDefault();
+    const newtodo = {
+      id: nanoid(),
+      title: title,
+      isCompleted: false
+    }
+    console.log(newtodo)
+    const copytodo=[...todo];
+    copytodo.push(newtodo);
+    settodos(copytodo)
+    settitle("")
+  }
+  const rendertodo=todo.map(todo=>{
+    return <li key={todo.id}>{todo.title}</li>
+  })
   return (
     <div>
       <h1>Create Task</h1>
       <br />
-      <form>
+      <form onSubmit={Submithandler}>
         <input
-        onChange={(e)=>console.log(e)}
-        type="text" placeholder="title" />
+          onChange={(e) => settitle(e.target.value)}
+          value={title}
+          type="text" placeholder="title" />
         <br />
-        <br />
-        <input type="checkbox" />Completed
-        <br />
-        <br />
+        {/*<br />
+        <input onChange={(e)=>console.log(e.target.checked)} type="checkbox" />Completed
+        <br /> 
+
+        <br />*/}
         <button>create todo</button>
       </form>
+      <hr />
+      <h1>Pending ToDos</h1>
+      <ol>{rendertodo}</ol>
     </div>
   )
 }
