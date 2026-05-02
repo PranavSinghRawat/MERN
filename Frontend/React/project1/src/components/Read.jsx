f919bb6768d86133835b2ff9d286fe977431b80d
MERN <pranavrawat0514@gmail.com>
2026-05-02 11:32:10 +0530

    Deleteting and completing this todo and learning styling how to do in react

--- Frontend/React/project1/src/components/Read.jsx
+++ Frontend/React/project1/src/components/Read.jsx
@@ -1,12 +1,16 @@
 const Read = (props) => {
     const todo=props.todo;
     const settodos=props.settodos;
+    const deletehandler=(id)=>{
+        const filteredtodo = todo.filter(todo=>todo.id!==id)
+        settodos(filteredtodo)
+    }
     const rendertodo = todo.map(todo => {
-        return <li key={todo.id}>{todo.title}</li>
+        return <li key={todo.id}>{todo.title}  |  <span onClick={()=>deletehandler(todo.id)}>delete</span></li>
     })
     return (
         <div>
-            <h1>Pending ToDos</h1>
+            <h1 style={{color: " tomato"}}>Pending ToDos</h1>
             <ol>{rendertodo}</ol>
         </div>
     )
