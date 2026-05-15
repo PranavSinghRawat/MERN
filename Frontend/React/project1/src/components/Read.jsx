1398abf2672b4db1de4fa04beb91722100d8f3c9
Pranav Rawat <pranavrawat0514@gmail.com>
2026-05-15 20:44:20 +0530

    styling in react

--- Frontend/React/project1/src/components/Read.jsx
+++ Frontend/React/project1/src/components/Read.jsx
@@ -1,16 +1,17 @@
+import "./Read.css"
 const Read = (props) => {
-    const todo=props.todo;
-    const settodos=props.settodos;
-    const deletehandler=(id)=>{
-        const filteredtodo = todo.filter(todo=>todo.id!==id)
+    const todo = props.todo;
+    const settodos = props.settodos;
+    const deletehandler = (id) => {
+        const filteredtodo = todo.filter(todo => todo.id !== id)
         settodos(filteredtodo)
     }
     const rendertodo = todo.map(todo => {
-        return <li key={todo.id}>{todo.title}  |  <span onClick={()=>deletehandler(todo.id)}>delete</span></li>
+        return <li key={todo.id}>{todo.title}  |  <span onClick={() => deletehandler(todo.id)}>delete</span></li>
     })
     return (
         <div>
-            <h1 style={{color: " tomato"}}>Pending ToDos</h1>
+            <h1 className="read_list_heading">Pending ToDos</h1>
             <ol>{rendertodo}</ol>
         </div>
     )
